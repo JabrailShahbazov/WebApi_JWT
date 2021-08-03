1eccf776023ca2812b886302e36823b53246c571
Jabrail Shahbazov <cebrail.shahbazov@gmail.com>
2021-08-03 22:45:18 +0400

    Shared ErrorDto.csn hazirlandi

---
+++ AuthServer.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AuthServer.Shared.Dtos
+{
+    public class ErrorDto
+    {
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+
+        public List<string> Errors { get; private set; }
+
+        public bool IsShow { get; private set; }
+    }
+}
