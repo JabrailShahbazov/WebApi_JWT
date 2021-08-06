07c91f2f322e67c97a4b92634c9efdb17f473534
Jabrail Shahbazov <cebrail.shahbazov@gmail.com>
2021-08-07 01:42:17 +0400

    Random Refresh token Duzletim

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using AuthServer.Shared.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOption _tokenOption;
+
+        public TokenService(
+            UserManager<UserApp> userManager,
+            IOptions<CustomTokenOption> options)
+        {
+            _userManager = userManager;
+            _tokenOption = options.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+            using var random = RandomNumberGenerator.Create();
+            random.GetBytes(numberByte);
+            return Convert.ToBase64String(numberByte);
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
