8f8ef30f26e479d530c44045ad981f968d16246c
Jabrail Shahbazov <cebrail.shahbazov@gmail.com>
2021-08-07 00:46:39 +0400

    SignInService yazildi

---
+++ AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthServer.Service.Services
+{
+    internal static class SignService
+    {
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
